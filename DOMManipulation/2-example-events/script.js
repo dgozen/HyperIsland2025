6edf830fd7a8f96afeb067c49d21b88bf346f9d8
dgozen <nazduyguakyol@gmail.com>
2025-09-26 20:14:59 +0200

    feat: dom exercises

---
+++ DOMManipulation/2-example-events/script.js
@@ -0,0 +1,25 @@
+// KEEP IN MIND: element.addEventListener("eventType", function)
+
+// Mouse
+// grab the elements first for the button and the hover box
+
+// 1) then add event listeners for click, mouseover, and mouseout
+// 2) use .textContent to change text
+// 3) use .style.borderColor to change border color
+
+// Keyboard & Input
+// 1) grab the elements first for the input and the live output
+// 2) add an event listener for input, you will want the .value property
+// 3) use .textContent to change text
+// 4) PRO TIP: use .trim() to remove whitespace
+
+// Form
+// 1) grab the elements first for the form, email input, and form status
+// 2) add an event listener for submit
+// 3)  PRO TIP: use e.preventDefault() to stop the form from submitting
+// 4) use .textContent for form status to say "Thanks, [email]!"
+
+// Page / Window
+// 1) grab the element first for the window size
+// 2) create a function to update the size text you will need to use window.innerWidth and window.innerHeight
+// 3) add event listeners for load and resize
---
+++ DOMManipulation/3-more-exercises/script.js
@@ -0,0 +1,22 @@
+// Grab targets
+
+// Grab buttons
+
+// 1) Change the title text
+// use textContent to change the text
+
+// 2) Highlight all list items
+// use forEach() to loop through listItems
+// use classList and add() method
+
+// 3) Add a new list item
+// use createElement() to create a new li element
+// use text.content to add text to the new li
+// use appendChild() to add the new li to colorList
+
+// 4) Remove the last list item
+// use items.length to grab the correct element
+// use remove() method to remove the element
+
+// 5) Toggle dark theme on container
+// use classList and toggle() method
